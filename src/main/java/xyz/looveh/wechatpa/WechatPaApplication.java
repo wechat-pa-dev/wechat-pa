7c45082413705ade797bbb7840e5f8410da2a44b
Looveh <965556351@qq.com>
2019-03-22 10:02:35 +0800

    项目初始化

---
+++ src/main/java/xyz/looveh/wechatpa/WechatPaApplication.java
@@ -0,0 +1,13 @@
+package xyz.looveh.wechatpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatPaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatPaApplication.class, args);
+    }
+
+}
